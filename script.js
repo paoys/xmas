bc1b18efc68b3b11e08f1cd7cc7881d129b12c07
Jaspher <119644835+paoys@users.noreply.github.com>
2023-12-24 21:09:02 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const card = document.querySelector('.card');
+    const backgroundMusic = document.getElementById('backgroundMusic');
 
-card.addEventListener('click', function() {
-    card.classList.toggle('open');
-});
+    card.addEventListener('click', function() {
+        card.classList.toggle('open');
+        if (card.classList.contains('open')) {
+            backgroundMusic.play();
+        } else {
+            backgroundMusic.pause();
+        }
+    });
